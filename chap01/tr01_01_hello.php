710dffe388855e2de58628afd42aae13daee0c42
bignyokki <shiro5989@yahoo.co.jp>
2022-07-16 22:58:36 +0900

    print文

---
+++ chap01/tr01_01_hello.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHPの基本</title>
+    </head>
+
+    <body>
+
+        <?php
+            /* <?php ~ ?> で囲まれた範囲にphpの記述をする */
+            // print命令は指定された文字列を表示するための命令です。
+            print 'こんにちは、世界！ <br>';
+            print 'こんにちは、皆さん！ ';
+        ?>
+
+        <br>
+
+        <?php
+            // print命令の省略形
+        ?>
+        <?='print命令は省略形で記述することができます。' ?><br>
+        <?='こんにちは、皆さん！！！！' ?>
+
+    </body>
+
+</html>
